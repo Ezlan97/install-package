91c79a9cc385965188e5aba1ab0d7d18d1519223
Ezlan <eizs798@gmail.com>
2018-09-04 19:22:18 +0800

    Feat | alpha stage not tested yet

---
+++ README.md
@@ -0,0 +1,11 @@
+# install-package
+
+## Bash script to 
+1. Setup laravel enviroment (nginx, composer, php, valet, mysql)
+2. Generate ssh key
+3. Update & upgrade pakcage
+4. Install driver
+5. Install zsh shell
+
+# disclaimer
+right now only working on ubuntu based distro or debian.
---
+++ install.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+echo "Created by Ezlan97"
+
+echo "Choose your package [laravel/ssh/driver/zsh/update&upgrade]"
+
+read input
+
+if [$input == "laravel"]; then
+    echo "installing mysql..."
+        sudo apt mysql-server -y
+
+    echo "installing php..."
+        sudo apt install -y php7.2 libapache2-mod-php7.2 php7.2-cli php7.2-common php7.2-mbstring php7.2-gd php7.2-intl php7.2-xml php7.2-mysql php7.2-zip php7.2-fpm php7.2-bcmath php7.2-curl
+
+    echo "installing composer..."
+        sudo apt install curl -y
+        curl -sS https://getcomposer.org/installer | php
+        sudo mv composer.phar /usr/local/bin/composer
+        chmod +x /usr/local/bin/composer
+
+    echo "installing valet..."
+        sudo apt-get install network-manager libnss3-tools jq xsel
+        sudo service apache2 stop
+        composer global require cpriego/valet-linux
+        export PATH=$PATH:$HOME/.config/composer/vendor/bin
+        valet install
+        echo "enter domain for valet"
+        read domain
+        valet domain $domain
+
+    echo "installing laravel installer..."
+        composer global require laravel/installer
+
+    echo "Done! your laravel enviroment had been setup"
+
+elif [$input == "ssh"]; then
+    echo "enter your email"
+    echo "generating your ssh key..."
+        ssh-keygen -t rsa -b 4096 -C "eizs798@gmail.com"
+        eval "$(ssh-agent -s)"
+        ssh-add ~/.ssh/id_rsa
+        hash sclip &>/dev/null && echo "xclip installed, continue copy ssh key" || sudo apt install xclip
+        xclip -sel clip < ~/.ssh/id_rsa.pub
+    echo "done! copied your ssh key"
+    echo "for later use just run this command : xclip -sel clip < ~/.ssh/id_rsa.pub"
+
+elif [$input == "driver"]; then
+    echo "installing driver packages..."
+        sudo ubuntu-drivers autoinstall
+    echo "Done! all drivers installed"
+
+elif [ $input == "update&upgrade" ]; then
+    echo "check for update and upgrade package"
+        sudo apt update && sudo apt upgrade
+    echo "done!"
+    
+else
+    echo "no match found"
+    echo "bye!"
+fi
