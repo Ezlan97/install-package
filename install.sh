04fd2a90286301ca469ad3c75523e65438bed37f
ez_lan <33053250+Ezlan97@users.noreply.github.com>
2018-09-05 08:44:12 +0000

    Fix | install mysql & typo

--- install.sh
+++ install.sh
@@ -15,12 +15,12 @@ read input
 # laravel
 if [ $input == "1" ]; then
     echo "installing mysql..."
-        sudo apt -y mysql-server
+        sudo apt install -y mysql-server
 
     echo "installing php..."
-        sdudo apt install -y python-software-properties
+        sudo apt install -y python-software-properties
         add-apt-repository ppa:ondrej/php
-        apt-get install -y php7.2
+        sudo apt install -y php7.2
         sudo apt install -y libapache2-mod-php7.2 php7.2-cli php7.2-common php7.2-mbstring php7.2-gd php7.2-intl php7.2-xml php7.2-mysql php7.2-zip php7.2-fpm php7.2-bcmath php7.2-curl
 
     echo "installing composer..."
