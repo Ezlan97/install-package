9987c874e5bda1a10a25daf40731cf2eb0f506d9
ez_lan <33053250+Ezlan97@users.noreply.github.com>
2018-09-05 08:39:13 +0000

    Fix | error install php on ubuntu 18.04

--- install.sh
+++ install.sh
@@ -18,7 +18,10 @@ if [ $input == "1" ]; then
         sudo apt mysql-server -y
 
     echo "installing php..."
-        sudo apt install -y php7.2 libapache2-mod-php7.2 php7.2-cli php7.2-common php7.2-mbstring php7.2-gd php7.2-intl php7.2-xml php7.2-mysql php7.2-zip php7.2-fpm php7.2-bcmath php7.2-curl
+        sdudo apt install -y python-software-properties
+        add-apt-repository ppa:ondrej/php
+        apt-get install -y php7.2
+        sudo apt install -y libapache2-mod-php7.2 php7.2-cli php7.2-common php7.2-mbstring php7.2-gd php7.2-intl php7.2-xml php7.2-mysql php7.2-zip php7.2-fpm php7.2-bcmath php7.2-curl
 
     echo "installing composer..."
         sudo apt install curl -y
