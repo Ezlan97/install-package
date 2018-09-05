3995943b84f5374e9cfc816f2675b55f4b57ef51
ez_lan <33053250+Ezlan97@users.noreply.github.com>
2018-09-05 08:53:06 +0000

    Fix | install php on ubuntu

--- install.sh
+++ install.sh
@@ -23,8 +23,8 @@ if [ $input == "1" ]; then
         sudo apt install python-software-properties
         add-apt-repository ppa:ondrej/php
         sudo apt-get update
-        sudo apt install  php7.2
-        sudo apt install libapache2-mod-php7.2 php7.2-cli php7.2-common php7.2-mbstring php7.2-gd php7.2-intl php7.2-xml php7.2-mysql php7.2-zip php7.2-fpm php7.2-bcmath php7.2-curl
+        sudo apt install php7.2
+        sudo apt install php-pear php7.2-curl php7.2-dev php7.2-gd php7.2-mbstring php7.2-zip php7.2-mysql php7.2-xml
 
     echo "installing composer..."
         sudo apt install curl -y
