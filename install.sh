a51e595a034035dc1272d288f8d01f4286845a9a
eizlan <eizs798@gmail.com>
2019-11-09 12:24:53 +0800

    Fix | disable vs code for now

--- install.sh
+++ install.sh
@@ -95,12 +95,12 @@ elif [ $input == "6" ]; then
 
     echo "done! installed docker"
 elif [ $input == "7" ]; then
-    echo "installing Visual Studio Code"
-        curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg;
-        sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg;
-        sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list';
-        sudo apt-get update;
-        sudo apt-get install code;
+    # echo "installing Visual Studio Code"
+    #     curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg;
+    #     sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg;
+    #     sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list';
+    #     sudo apt-get update;
+    #     sudo apt-get install code;
     echo "installing Android Studio"
         sudo apt install snapd snapd-xdg-open -y
         sudo snap install android-studio --classic
